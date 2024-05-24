50f2f03a446f03d9ada58761aaa0c6f175e28b94
noodle2601 <vankhanhboysss2601@gmail.com>
2024-05-24 17:19:37 +0700

    m

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,6 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
+
 
 let getHomePage = async (req, res) => {
     try {
@@ -19,8 +21,9 @@ let getCRUD = (req, res) => {
     return res.render('crud.ejs');
 }
 
-let postCRUD = (req, res) => {
-    console.log(req.body);
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
     return res.send('post crud from server');
 }
 
